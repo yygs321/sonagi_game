b2687f03087171529edf24764234c68828888973
somin <83453597+yygs321@users.noreply.github.com>
2021-05-25 00:26:48 +0900

    랭킹화면 구현

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GameMainView Game_Main = new GameMainView();
+		Ranking rank= new Ranking();
+	}
+
+}
