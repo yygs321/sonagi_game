ad7f7b4d7efa2d0fa0934d170af6aa37fc7d617a
HaeunDO <HaeunDO>
2021-06-12 21:19:34 +0900

    로그인과 인게임 타이틀이 달라서 맞추었습니다

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main extends JFrame {
 	
 	public void playGame(GameMainView login, String ID, String Level) throws IOException, ParseException{
 		login.dispose();
-        JFrame myFrame = new JFrame("산성비 게임"); // JFrame 객체를 생성한다
+        JFrame myFrame = new JFrame("단어 맞추기 게임"); // JFrame 객체를 생성한다
         myFrame.add(new Gui(ID, Level));
         myFrame.setSize(800, 596); // JFrame 사이즈 정의
         myFrame.setLocationRelativeTo(null); // JFrame을 모니터의 중앙에 띄우게 한다
